66c9fc90320a896c0c1985591abe2ccb835f404a
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-03-07 12:39:11 +0100

    Amelioration of function to insert in DB from JSON, function to remove all documents added

--- server/database/index.js
+++ server/database/index.js
@@ -5,30 +5,46 @@ const MONGODB_URI='mongodb+srv://admin-user:ULTRA_password_92@clear-fashion-clus
 const MONGODB_DB_NAME = 'clearfashion';
 const fs=require('fs');
 
-async function main (mongo_uri, mongo_db_name){
-    const client = await MongoClient.connect(mongo_uri, {'useNewUrlParser': true});
-    const db =  client.db(mongo_db_name);
-
-
-
-    const collection = db.collection('products');
-    await insertFromJSON(collection);
-    client.close();
-}
 
 function getJSONProducts (){
+    
+}
+
+async function insertProductsFromJSON (mongo_uri, mongo_db_name){
+    const client = await MongoClient.connect(mongo_uri, {'useNewUrlParser': true});
+    const db =  client.db(mongo_db_name);
+    const collection = db.collection('products');
+
     return fs.readFile("database/products.json", (err,jsonString)=>{
         if (err){
             console.log("Error reading the file");
             return 
         }
-            const res=JSON.parse(jsonString);
+            const products=JSON.parse(jsonString);
             console.log('Products successfully got');
-            console.log(res[0]);
-            return res;
-    });
+            collection.insertMany(products)
+            .then(
+                res => console.log(`Successfuly inserted ${res.result.n} documents`),
+                error => console.error(error)
+            );
+            client.close();
+    });    
+}
+
+async function deleteAllProducts (mongo_uri, mongo_db_name){
+    const client = await MongoClient.connect(mongo_uri, {'useNewUrlParser': true});
+    const db =  client.db(mongo_db_name);
+    const collection = db.collection('products');
+    collection.remove({})
+        .then(
+            res => console.log(res.result),
+            error => console.error(error)
+        )
+    client.close()
 }
 
 
-//main(MONGODB_URI,MONGODB_DB_NAME);
-getJSONProducts();
+
+
+insertProductsFromJSON(MONGODB_URI,MONGODB_DB_NAME);
+// deleteAllProducts(MONGODB_URI, MONGODB_DB_NAME);
