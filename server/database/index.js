61c4156e4dd640f8b563d67450f2c69b7ca7e4a5
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-03-08 20:17:42 +0100

    MongoCluster class initiated + packages for api installed

--- server/database/index.js
+++ server/database/index.js
@@ -1,16 +1,35 @@
 'user strict';
 
 const {MongoClient} = require('mongodb');
-const MONGODB_URI='mongodb+srv://admin-user:ULTRA_password_92@clear-fashion-cluster.nnulq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const MONGODB_DB_NAME = 'clearfashion';
+ // TODO put this information in a .config file 
+ 
+const MONGODB_URI ="mongodb+srv://admin-user:ULTRA_password_92@clear-fashion-cluster.nnulq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const MONGODB_DB_NAME = "clearfashion"
+const config = require("../config");
 const fs=require('fs');
 
 
 
-async function insertProductsFromJSON (mongo_uri, mongo_db_name){
-    const client = await MongoClient.connect(mongo_uri, {'useNewUrlParser': true});
-    const db =  client.db(mongo_db_name);
-    const collection = db.collection('products');
+class MongoCluster {
+    constructor({mongo_uri, mongo_db_name}){
+        this.mongo_uri=mongo_uri;
+        this.mongo_db_name=mongo_db_name;
+        this.db=null;
+        this.client=null;
+        this.collection=null;
+    }
+
+    //TODO function to connect to the database and set the client, the db and the collection if not already connected
+
+    //TODO function to instert an array of product in the DB and not stopping if the product is already in the DB
+
+    //TODO function to make a query 
+
+
+    async insert (){
+        const client = await MongoClient.connect(mongo_uri, {'useNewUrlParser': true});
+        const db =  client.db(mongo_db_name);
+        const collection = db.collection('products');
 
     return fs.readFile("database/products.json", (err,jsonString)=>{
         if (err){
@@ -86,6 +105,7 @@ async function getProductSortedByPrice(mongo_uri, mongo_db_name) {
     return products;
 }
 
+}
 
 
 
