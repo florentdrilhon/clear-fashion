b7479d753b554fe93785f7a58841d1921c732379
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-03-15 12:02:29 +0100

    Functions to connect, insert and close added in class Mongocluster

--- server/database/index.js
+++ server/database/index.js
@@ -5,13 +5,13 @@ const {MongoClient} = require('mongodb');
  
 const MONGODB_URI ="mongodb+srv://admin-user:ULTRA_password_92@clear-fashion-cluster.nnulq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const MONGODB_DB_NAME = "clearfashion"
-const config = require("../config");
+//const config = require("../config");
 const fs=require('fs');
 
 
 
 class MongoCluster {
-    constructor({mongo_uri, mongo_db_name}){
+    constructor(mongo_uri, mongo_db_name){
         this.mongo_uri=mongo_uri;
         this.mongo_db_name=mongo_db_name;
         this.db=null;
@@ -19,13 +19,77 @@ class MongoCluster {
         this.collection=null;
     }
 
-    //TODO function to connect to the database and set the client, the db and the collection if not already connected
+    // function to connect to the database and set the client, the db and the collection if not already connected
+    async connect() {
+        try{
+            if (this.db != null){
+                // db already connected
+                console.log("Already connected");
+            }
+            else {
+                // db not connected, initiating connection
+                this.client = await MongoClient.connect(this.mongo_uri, {'useNewUrlParser': true});
+                this.db =  this.client.db(this.mongo_db_name);
+                this.collection = this.db.collection('products');
+                console.log("Connected");
+            }
+        } catch(error){
+            console.log("Error in connection to the mongo client :", error);
+        }
+    };
 
     //TODO function to instert an array of product in the DB and not stopping if the product is already in the DB
+    async insert(products){
+        //take the array as arg
+        //check if the client is connected, connect if not
+        this.connect();
+        try {
+            //try insert the products
+            const result=await this.collection.insertMany(products, {'ordered': false});
+            // return the result
+            return result;
+        } catch(error){
+            //catch error if error
+            console.log("Error inserting the products :", error);
+            console.log("Storing the products in a JSON file");
+            // put the products in a JSON file not to lose them
+            fs.writeFileSync('products.json', JSON.stringify(products));
+            return {
+                'insertedCount': error.result.nInserted
+             };
+        }
+    }
+    //TODO function to make a query
 
-    //TODO function to make a query 
+    
+    
+    // function to close the connection
+    async close(){
+        try {
+            await this.client.close();
+            console.log("Connection closed")
+        } catch(error){
+            console.log("Error closing the connection :", error);
+        }
+    }
+};
 
 
+
+let Mongocluster = new MongoCluster(MONGODB_URI, MONGODB_DB_NAME);
+
+async function test(Mongocluster){
+    const products=[{brand:"LAcoste", name: "le polo stylax"}, {brand:"Tu le c", name: "eh la bb"}];
+    await Mongocluster.connect();
+    const result=await Mongocluster.insert(products);
+    console.log(result);
+    Mongocluster.close();
+}
+
+test(Mongocluster);
+
+
+/*
     async insert (){
         const client = await MongoClient.connect(mongo_uri, {'useNewUrlParser': true});
         const db =  client.db(mongo_db_name);
@@ -105,7 +169,7 @@ async function getProductSortedByPrice(mongo_uri, mongo_db_name) {
     return products;
 }
 
-}
+
 
 
 
@@ -116,4 +180,6 @@ async function getProductSortedByPrice(mongo_uri, mongo_db_name) {
 // insertProductsFromJSON(MONGODB_URI,MONGODB_DB_NAME);
 // deleteAllProducts(MONGODB_URI, MONGODB_DB_NAME);
 
-let test=getProductByBrand(MONGODB_URI, MONGODB_DB_NAME, "DEDICATED");
+//let test=getProductByBrand(MONGODB_URI, MONGODB_DB_NAME, "DEDICATED");
+
+*/
